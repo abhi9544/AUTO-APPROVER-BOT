d0beeed7a29a8e4d15f5ab3ce65958a9af590fed
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-06-03 20:16:57 +0530

    Create beta.py

---
+++ beta.py
@@ -0,0 +1,36 @@
+
+
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
+
+pr0fess0r_99=Client(
+    "Auto Approved Bot",
+    bot_token = environ["BOT_TOKEN"],
+    api_id = int(environ["API_ID"]),
+    api_hash = environ["API_HASH"]
+)
+
+CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
+TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
+
+@pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
+async def start(client: pr0fess0r_99, message: Message):
+    approvedbot = await client.get_me() 
+    button = [[ InlineKeyboardButton("📦 Repo", url="https://github.com/Jeolpaul/AUTO-APPROVER-BOT"), InlineKeyboardButton("Updates 📢", url="https://t.me/beta_bot_updates") ],
+              [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
+    await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Made By [BETA BOTZ](https://t.me/beta_bot_updates)🚶||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+
+@pr0fess0r_99.on_chat_join_request(filters.group & filters.chat(CHAT_ID) if CHAT_ID else filters.group)
+async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
+    chat=message.chat # Chat
+    user=message.from_user # User
+    print(f"{user.first_name} Joined 🤝") # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
+    #   print("Welcome....")
+
+print("Auto Approved Bot")
+pr0fess0r_99.run()
